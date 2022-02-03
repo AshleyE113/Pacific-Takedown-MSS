298cd09321944c58d1bb291d7161270535afaff6
Nycelly <dkr264@nyu.edu>
2022-02-03 18:54:41 -0500

    Movement Animations Implemented

--- Pacific Takedown Unity/Assets/PlayerController.cs
+++ Pacific Takedown Unity/Assets/PlayerController.cs
@@ -7,11 +7,15 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 5;
     private Rigidbody2D rb;
 
+    private Animator animator;
+    private string currentState;
+
     Vector2 movement; 
     // Start is called before the first frame update
     void Start()
     {
       rb = gameObject.GetComponent<Rigidbody2D>();
+      animator = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -19,10 +23,23 @@ public class PlayerController : MonoBehaviour
     {
       movement.x = Input.GetAxisRaw("Horizontal");
       movement.y = Input.GetAxisRaw("Vertical");
+      animator.SetFloat("Horizontal", movement.x);
+      animator.SetFloat("Vertical", movement.y);
     }
 
     private void FixedUpdate()
     {
       rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    if (movement.x != 0 || movement.y != 0) { ChangeAnimationState("Movement"); }
+    else { ChangeAnimationState("Lea_Idle_2"); }
+    }
+
+    private void ChangeAnimationState(string newState)
+    {
+      if (currentState == newState) return;
+
+      animator.Play(newState);
+
+      currentState = newState;
     }
 }
