169ffe2d7957c90e683b0ffbda896559f7f4ee20
Nycelly <dkr264@nyu.edu>
2022-02-04 16:18:17 -0500

    Movement Code WiP 2

--- Pacific Takedown Unity/Assets/PlayerController.cs
+++ Pacific Takedown Unity/Assets/PlayerController.cs
@@ -9,52 +9,85 @@ public class PlayerController : MonoBehaviour
 
     private Animator animator;
     private string currentState;
+    private bool safeToUpdateDir = true;
 
     Vector2 movement;
-    Vector2 previousDirection;
-  private bool directionCountdown;
+    Vector2 playerFacing;
+    public float directionResetTime=0.25f;
+    private bool resetDirCooldownRunning;
     // Start is called before the first frame update
   void Start()
     {
       rb = gameObject.GetComponent<Rigidbody2D>();
       animator = gameObject.GetComponent<Animator>();
+
+      //when it starts it sets the default facing to North
+      playerFacing.x = 0;
+      playerFacing.y = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
+      //input
+      movement.x = Input.GetAxisRaw("Horizontal");
+      movement.y = Input.GetAxisRaw("Vertical");
 
+      if (movement.x == 0 && movement.y == 0)
+      {
+        //if there is no input, it does nothing...
+      }
+      else
+      {
+        //set direction before normalising
+        updatePlayerDir(movement);
+        movement = movement.normalized;
+
+        playerFacing.x = movement.x;
+        playerFacing.y = movement.y;
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+      }
+    }
+
+  void updatePlayerDir(Vector2 movement)
+  {
+    if (safeToUpdateDir)
+    {
+      animator.SetFloat("PreviousHorizontal", playerFacing.x);
+      animator.SetFloat("PreviousVertical", playerFacing.y);
+    }
+
+    //animator.SetFloat("Direction", (int)playerFacing);
+    safeToUpdateDir = false;
+    if (resetDirCooldownRunning == false) { StartCoroutine(resetDirCooldown()); resetDirCooldownRunning = true; }
   }
 
-    private void FixedUpdate()
+  IEnumerator resetDirCooldown()
+  {
+    yield return new WaitForSeconds(directionResetTime);
+    safeToUpdateDir = true;
+    resetDirCooldownRunning = false;
+  }
+
+  private void FixedUpdate()
     {
 
+    if (movement.x == 0 && movement.y == 0)
+    {
 
-    float horizontalInput = Input.GetAxis("Horizontal");
-    float verticalInput = Input.GetAxis("Vertical");
-    Vector2 inputVector = new Vector2(horizontalInput, verticalInput);
-    inputVector = Vector2.ClampMagnitude(inputVector, 1);
-    Vector2 movement = inputVector * moveSpeed;
-    // check if user let go of the stick; if so, reset the input bounce control
-    animator.SetFloat("Horizontal", movement.x);
-    animator.SetFloat("Vertical", movement.y);
-    rb.MovePosition(rb.position + movement * Time.fixedDeltaTime);
-
-
-    /*
-        if (movement.x != 0 || movement.y != 0) 
-      { 
-        ChangeAnimationState("Movement"); 
-      }
-        else 
-      { 
-        ChangeAnimationState("Idle");
-        //animator.SetFloat("PreviousHorizontal", previousDirection.x);
-        //animator.SetFloat("PreviousVertical", previousDirection.y);
-      }
-    */
-
-
+      ChangeAnimationState("Stop");
+    }
+    else
+    {
+      //movement
+      rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+      ChangeAnimationState("Movement");
+      Debug.Log(movement);
+      Debug.Log(playerFacing);
+    }
   }
 
   private void ChangeAnimationState(string newState)
