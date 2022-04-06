e6f8fd80f04510eaa36596af029968a18752b763
Nycelly <dkr264@nyu.edu>
2022-04-06 12:43:24 -0400

    Fixed Enemy Pathfinding

--- Pacific Takedown Unity/Assets/Scripts/EnemyScripts/EnemyAI.cs
+++ Pacific Takedown Unity/Assets/Scripts/EnemyScripts/EnemyAI.cs
@@ -186,7 +186,7 @@ public class EnemyAI : MonoBehaviour
                         reachedEndOfPath = false;
                     }
 
-                    //direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
+                    direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
                     Vector2 force = direction * speed * Time.deltaTime;
                     rb.AddForce(force);
                     float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
