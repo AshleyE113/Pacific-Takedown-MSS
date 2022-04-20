00731417862cbe3361c57b98a2e87e3ca9d652de
Ashley <ase330@nyu.edu>
2022-04-20 01:32:46 -0400

    //WIP On Text In StartScreen & trying to spot that knockback bug

---
+++ Pacific Takedown Unity/Assets/Scripts/StartScreenScript/CinematicOpening.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CinematicOpening : MonoBehaviour
+{
+
+    [SerializeField] List<string> openingLines;
+    [SerializeField] TMP_Text textHolder;
+    [SerializeField] Image goSprite;
+    bool isTyping = false;
+    bool cancelTyping = false;
+    public float typeSpeed = 0.3f;
+    int currentIndex = 0;
+
+    //Other varis
+    Color col = Color.white;
+    float alphaVal = 255f;
+    float lerpVal = 0.5f;
+    Color objColor;
+
+    void Start()
+    {
+        objColor = goSprite.color;
+    }
+
+    void Update()
+    {
+        RunThroughText();
+    }
+
+    void RunThroughText()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) //Allows the player to go through the script.
+        {
+            if (!isTyping)
+            {
+                if (currentIndex < openingLines.Count)
+                {
+                    Debug.Log(currentIndex);
+                    StartCoroutine(TextScroll(openingLines[currentIndex]));
+                    currentIndex += 1;
+
+                    if (currentIndex == 3)
+                    {
+                        Color.Lerp(objColor, col, lerpVal);
+                        lerpVal += Time.deltaTime;
+
+                        if (objColor.a > 0)
+                        {
+                            objColor.a = (alphaVal - 0.9f);
+                        }
+                    }
+                }
+                
+
+            }
+
+
+        }
+    }
+
+
+    private IEnumerator TextScroll(string lineOfText) //Gives it that one character at a timeffect...
+    {
+        int letter = 0;
+        textHolder.text = "";
+        isTyping = true;
+        cancelTyping = false;
+        yield return new WaitForSeconds(typeSpeed);
+        while (isTyping && !cancelTyping && (letter < lineOfText.Length - 1))
+        {
+            textHolder.text += lineOfText[letter];
+            letter += 1;
+            yield return new WaitForSeconds(typeSpeed);
+        }
+        textHolder.text = lineOfText;
+        isTyping = false;
+        cancelTyping = false;
+    }
+}
+
