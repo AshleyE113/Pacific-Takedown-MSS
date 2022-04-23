acf8312da668031c81a76c233a9dd9b7d5c699d0
Josh Finkel <jlf581@nyu.edu>
2022-04-23 18:08:41 -0400

    Alarm gets visibly destroyed when hit

--- Pacific Takedown Unity/Assets/Scripts/ObstacleScripts/ComputerSpriteChange.cs
+++ Pacific Takedown Unity/Assets/Scripts/ObstacleScripts/ComputerSpriteChange.cs
@@ -8,6 +8,7 @@ public class ComputerSpriteChange : MonoBehaviour
     private SpriteRenderer myRenderer;
     private bool changed;
     public Sprite otherSprite; //define the sprite that I want to change to
+    public Animator myAnimator; //define the animator
 
     // Start is called before the first frame update
     void Start()
@@ -15,15 +16,22 @@ public class ComputerSpriteChange : MonoBehaviour
         myRenderer = GetComponent<SpriteRenderer>(); //getting my sprite renderer
     }
 
-     public void ChangeSprite()
-     { //if I collide with an enemy
-       myRenderer.sprite = otherSprite; //change my sprite to another sprite.
-       if (changed != true)
-       {
+    public void ChangeSprite()
+    { //if I collide with an enemy
+
+        if(myAnimator != null){ //disable animator
+            Debug.Log("ANIMATOR DETECTED");
+            myAnimator.enabled = false;
+        }
+
+        myRenderer.sprite = otherSprite; //change my sprite to another sprite.
+
+        if (changed != true) //shake camera and create particle effect
+        {
            CameraController.Shake(4f, 4f, 0.1f, 0.1f);
            FXManager.spawnEffect("electricity",gameObject,transform,quaternion.identity,false,new Vector2(0f,0f));
            changed = true;
-       }
+        }
 
      }
 
