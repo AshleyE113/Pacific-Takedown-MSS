0c12b3305382144080fd7bf3b65007fcca3c5e8a
Ashley <ase330@nyu.edu>
2022-05-02 12:51:59 -0400

    //Updated QP script and got Lvl1 back!

--- Pacific Takedown Unity/Assets/Scripts/UI/Window_QuestPointer.cs
+++ Pacific Takedown Unity/Assets/Scripts/UI/Window_QuestPointer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using CodeMonkey.Utils;
+using UnityEngine.SceneManagement;
 
 public class Window_QuestPointer : MonoBehaviour
 {
@@ -10,15 +11,24 @@ public class Window_QuestPointer : MonoBehaviour
     private RectTransform pointerRectTransform;
     GameObject roomExit;
     float roomExitx, roomExity;
+    [SerializeField] float pointerAngle;
 
-    private void Awake(){
-        uiCamera = GameObject.FindWithTag("MainCamera").GetComponent<Camera>();
+    private void Awake()
+    {
+
+        //uiCamera = GameObject.FindWithTag("MainCamera").GetComponent<Camera>();
         roomExit = GameObject.FindGameObjectWithTag("RoomExit");
         roomExitx = roomExit.transform.position.x;
         roomExity = roomExit.transform.position.y;
-
         targetPosition = new Vector3(roomExitx, roomExity, 0);
         pointerRectTransform = transform.Find("Pointer").GetComponent<RectTransform>();
+        SceneManager.sceneLoaded += OnSceneLoaded;
+
+    }
+
+    void OnSceneLoaded(Scene s, LoadSceneMode Add)
+    {
+        uiCamera = Camera.main;
     }
 
     private void Update()
@@ -26,10 +36,11 @@ public class Window_QuestPointer : MonoBehaviour
         float borderSize = 100f;
         Vector3 targetPositionScreenPoint = Camera.main.WorldToScreenPoint(targetPosition);
         bool isOffScreen = targetPositionScreenPoint.x <= borderSize || targetPositionScreenPoint.x >= Screen.width - borderSize || targetPositionScreenPoint.y <= borderSize || targetPositionScreenPoint.y >= Screen.height - borderSize;
-
+        isOffScreen = false;
+        print(isOffScreen);
         if (isOffScreen)
         {
-            RotatePointerTowardsTargetPosition();
+
 
             Vector3 cappedTargetScreenPosition = targetPositionScreenPoint;
             if (cappedTargetScreenPosition.x <= borderSize) cappedTargetScreenPosition.x = borderSize;
@@ -48,19 +59,19 @@ public class Window_QuestPointer : MonoBehaviour
             pointerRectTransform.position = pointerWorldPosition;
             pointerRectTransform.localPosition = new Vector3(pointerRectTransform.localPosition.x, pointerRectTransform.localPosition.y, 0f);
 
-            pointerRectTransform.localEulerAngles = Vector3.zero;
+            RotatePointerTowardsTargetPosition();
         }
     }
 
     private void RotatePointerTowardsTargetPosition()
     {
         Vector3 toPosition = targetPosition;
-        Vector3 fromPosition = Camera.main.transform.position;
+        Vector3 fromPosition = uiCamera.transform.position;
         fromPosition.z = 0f;
         Vector3 dir = (toPosition - fromPosition).normalized;
         float angle = UtilsClass.GetAngleFromVectorFloat(dir);
-        pointerRectTransform.localEulerAngles = new Vector3(0, 0, angle);
+        pointerRectTransform.localEulerAngles = new Vector3(0, 0, angle + pointerAngle);
+
+        print($"Dir:{dir} Angle:{angle}");
     }
-
-
 }
